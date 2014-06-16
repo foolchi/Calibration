a2ef8986f01862505938ad1c5a7294075a115c96
foolchi <foolchi@outlook.com>
2014-06-16 09:45:05 +0200

    original

---
+++ calibration.pro
@@ -0,0 +1,8 @@
+TEMPLATE = app
+CONFIG += console
+CONFIG -= app_bundle
+CONFIG -= qt
+INCLUDEPATH += /opt/ros/indigo/include \
+         /usr/include/pcl-1.7
+SOURCES += main.cpp
+
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ);
+
+    cloud->width = 15;
+    cloud->height = 1;
+    cloud->points.resize(cloud->width * cloud->height);
+
+    for(size_t i = 0; i < cloud->points.size(); i++){
+    }
+}
+
